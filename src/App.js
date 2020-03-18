f7f5b4caa44832de9ff9ab288be05ba5dd0a2638
Brayden DeLoach <brayden@deloach.us>
2020-03-18 18:23:35 -0500

    Removed a lot of starter react code. App.js now retrieves from api and displays first names as a test.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import API from "./utils/API";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  state = {
+    employees: []
+  };
+
+  componentDidMount(){
+    API.getUsers()
+    .then(result => this.setState({employees: result.data.results}))
+    .catch(err => console.log(err));
+  }
+
+  render() {
+    return(
+      <div>
+        {this.state.employees.map(element => <p key={element.id.value}>{element.name.first}</p>)}
+      </div>
+    );
+  }
 }
 
 export default App;
